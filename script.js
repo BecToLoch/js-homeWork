2c40f3452b39dffcd25b16ef659dbc3a9cd52976
bogdan <bogdandidyc91@gmail.com>
2026-03-27 00:57:01 +0300

    5 exercise

--- script.js
+++ script.js
@@ -1,40 +1,159 @@
-/*
-console.log(2 * 2 + 2);
+//let num = Number(prompt('введи число'))
 
-let sin54 = 54 * (Math.PI / 180)
-let cos16 = 16 * (Math.PI / 180)
+/*
+switch (true){
+  case(num > 0):
+    console.log('число положительное');
+    break
+  case(num < 0):
+    console.log('число отрицательное');
+    break
+  case(num = 0):
+    console.log('число рано нулю');
+    break
+  default:
+    console.log('это не число');
+}
+
+console.log(num);
 */
 
 /*
-const result = Math.sin(sin54) * Math.cos(cos16) ** 2
+const result =  num === '1' ? 'напишите число' 
+      : num > 0 ? 'число положительное' 
+      : num < 0 
+      ? 'число отрицательное'
+      : 'равно нулю'
 
-console.log(Math.ceil(result));
+console.log(result);
 */
 
 /*
-const part1 = 16 * Math.sqrt(13.2 * 71.90) / (2.4 / 7 ** 4)
-const part2 = 3 ** Math.sqrt(49)
-const multiplier = 2 ** 7
+if (num === Number('1')){
+  console.log('введите число');
+} else if (num > 0){
+  console.log('положительное');
+}else if (num < 0){
+  console.log('отрицательное');
+}else{
+  console.log('равно нулю');
+}
 
-const result = (part1 + part2) * multiplier
-
-console.log(Math.ceil(result));
-*/ 
+console.log(num);
+*/
 
 /*
-lеt a = 6
-let b = 5
-let result = ''
+let height = Number(prompt('введите рост'))
+let weight = Number(prompt('введите вес'))
 
-if (a % 2 === 0 ) result += ' a четное число'
-if (b % 2 === 0 ) result += ' b четное число'
 
-if (result !== ''){
-  console.log(result);
-}else {
-  console.log('четных нет');
+if (!height || isNaN(height) || !weight || isNaN(weight)){
+  console.log('введите коректное значение');
+}else{
+  if(height > 3) height = height / 100
+
+  const IMT = weight / height ** 2
+  console.log('Ваш ИМТ', IMT.toFixed(1));
+  
+  switch (true) {
+    case (IMT <= 18.5):
+      console.log('дефицит массы');
+      break;
+    case (IMT >= 18.5 && IMT <= 24.9):
+      console.log('здоровый диапозон');
+      break;
+    case (IMT >= 25.5 && IMT <= 29.9):
+      console.log('у вас предожирение');
+      break;
+    case (IMT >= 30):
+      console.log('у вас ожирение');
+      break;    
+    default:
+      console.log('ошибка расчета');
+  }
 }
 */
 
-const name = prompt ('Введите вше имя') || 'Guest'
-console.log('Hello', name);
+/*
+let month = Number(prompt('введите число месяца месяц'))
+let monthName
+
+if (!month || isNaN(month)){
+  console.log('введите коректно');
+}else{
+
+  switch (month) {
+    case 1:
+      monthName = 'январь';
+      break;
+    case 2:
+      monthName = 'февраль';
+      break;
+    case 3:
+      monthName = 'март';
+      break;
+    case 4:
+      monthName = 'апрель';
+      break;
+    case 5:
+      monthName = 'май';
+      break;
+    case 6:
+      monthName = 'июнь';
+      break;
+    case 7:
+      monthName = 'июль';
+      break;
+    case 8:
+      monthName = 'август';
+      break;
+    case 9:
+      monthName = 'сентябрь';
+      break;
+    case 10:
+      monthName = 'октябрь';
+      break;
+    case 11:
+      monthName = 'ноябрь';
+      break;
+    case 12:
+      monthName = 'декабрь';
+      break;                    
+    default:
+      monthName = 'неверный месяц' ;
+  }
+  console.log(monthName);
+}
+*/
+
+let discount
+let userStatus = prompt('введите ваш стасус')
+
+if (!userStatus || userStatus.trim() === ''){
+  console.log('введите статус');
+}else{
+
+  userStatus = userStatus.toLowerCase()
+
+  if (!['vip', 'dimond', 'gold'].includes(userStatus)){
+    console.log('здраствуйте гость');
+  }else{
+
+    switch (userStatus) {
+      case 'gold':
+        discount = '10%'
+        console.log(`здраствуйте ваша скидка ${discount}`);
+        break;
+      case 'dimond':
+        discount = '15%'
+        console.log(`здраствуйте сударь ваша скидка ${discount}`);
+        break;
+      case 'vip':
+        discount = '25%'
+        console.log(`здраствуйте очарование ваша скидка ${discount}`);
+        break;  
+      default:
+        break;
+    }
+  }
+}
