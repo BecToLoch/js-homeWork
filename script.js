45ddf00520ab20b589350f1b162792d92558ea60
bogdan <bogdandidyc91@gmail.com>
2026-04-07 16:56:08 +0300

    13 exercise

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 /*
-function hasEvenNumber(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      return true;
-    }
-  }  
-  return false;
+const user = {
+  name: 'Ivan',
+  age: 21,
+  hobbi: ['gaming', 'music', 'IT']
 }
 
+console.log(user);
 
-console.log(hasEvenNumber([1, 3, 4, 5])); // Ожидается: true
+for( let key in user){
+  if (Array.isArray(user[key])) {
+      console.log(`${key}: ${user[key].join(', ')}`);
+  }else {
+      console.log(`${key}: ${user[key]}`);
+  }
+}
 */
 
-/*
-function calculateAverage(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) { 
-    sum += numbers[i];
-    
-  }
-  return sum / numbers.length;
-  
+
+const user = {
+  name: 'Ivan',
+  age: 21,
+  hobbi: ['gaming', 'music', 'IT']
 }
 
-console.log(calculateAverage([2, 4, 6])); // Ожидается: 4
-*/
+user.name = 'Nikita'
+user.city = 'Moscow'
+delete user.hobbi
 
-console.log('feve');
+console.table(user);
 
-function findLargestNumber(arr) {
-
-  let largest = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      largest = arr[i];      
-    }
-  }
-  return largest;
- 
-}
-
-console.log(findLargestNumber([-10, -20, -30])); // Ожидается: -10
 
